7edb050332a443c5041df73586b3d54fd53ba773
Rahul Dubey <dubeyrahulrnc@gmail.com>
2023-02-13 17:20:11 +0530

    Sparated UI from BL

--- POS.UI/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    config:{
-      apiUrl:'https//example.com/api/' // TODO: Replace this URL with production one
-    }
-  };
--- POS.UI/src/app/models/item.ts
+++
@@ -1,7 +0,0 @@
-import { Size } from "../enums/size";
-
-export interface Item {
-    name: string,
-    price: number,
-    size: Size
-}
